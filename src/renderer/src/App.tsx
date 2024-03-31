18dab53a8b3748d0ac6a586c1dab39421884b697
fekoneko <55813967+fekoneko@users.noreply.github.com>
2024-03-31 22:03:18 +0300

    got all up and running

---
+++ src/renderer/src/App.tsx
@@ -0,0 +1,5 @@
+const App = () => {
+  return <div className="text-center">App</div>;
+};
+
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/renderer/**/*.{js,ts,jsx,tsx,html}'],
+  darkMode: 'class',
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
