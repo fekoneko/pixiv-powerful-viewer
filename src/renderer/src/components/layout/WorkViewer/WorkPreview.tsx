97b66ddfe54a88bf54163fe573f69c0007eec6a3
fekoneko <55813967+fekoneko@users.noreply.github.com>
2024-04-02 19:11:58 +0300

    work preview styles

--- src/renderer/src/components/layout/WorkViewer/WorkPreview.tsx
+++ src/renderer/src/components/layout/WorkViewer/WorkPreview.tsx
@@ -8,27 +8,38 @@ const WorkPreview = ({ work }: WorkPreviewProps) => {
   const [currentPage, setCurrentPage] = useState(0);
 
   return (
-    <div className="rounded-xl relative border-2 border-text/30 my-2 shadow-lg overflow-hidden flex items-center justify-center">
+    <div className="relative my-2 flex items-center justify-center overflow-hidden rounded-xl border-2 border-text/30 shadow-lg">
       {work?.assets?.length ? (
         <>
-          <img src={work.assets[currentPage]?.mediaPath} className="max-w-full max-h-full" />
+          <img src={work.assets[currentPage]?.mediaPath} className="z-10 max-h-full max-w-full" />
+
+          <div className="absolute flex h-full w-[9999px] justify-center">
+            <img
+              src={work.assets[currentPage]?.mediaPath}
+              className="h-full blur-md [transform:scale(1.2)]"
+            />
+            <div className="absolute size-full bg-background/30" />
+          </div>
+
           <button
-            className="absolute left-0 text-3xl p-4 h-full text-white"
+            className="absolute left-0 z-20 h-full w-1/3 p-4 text-left text-3xl text-white opacity-0 transition-opacity [background:radial-gradient(farthest-side_at_0_50%,#33335544,transparent_50%)] hover:opacity-100"
             onClick={() =>
               setCurrentPage((prev) => (prev > 0 ? prev - 1 : work.assets!.length - 1))
             }
           >
             {'<'}
           </button>
+
           <button
-            className="absolute right-0 text-3xl p-4 h-full text-white"
+            className="absolute right-0 z-20 h-full w-1/3 p-4 text-right text-3xl text-white opacity-0 transition-opacity [background:radial-gradient(farthest-side_at_100%_50%,#33335544,transparent_50%)] hover:opacity-100"
             onClick={() =>
               setCurrentPage((prev) => (prev < work.assets!.length - 1 ? prev + 1 : 0))
             }
           >
             {'>'}
           </button>
-          <p className="absolute top-0 text-white">
+
+          <p className="absolute top-0 z-20 h-10 w-full text-center text-white opacity-0 transition-opacity [background:radial-gradient(farthest-side_at_50%_0,#33335555,transparent)] [:hover>&]:opacity-100">
             {currentPage + 1} / {work.assets.length}
           </p>
         </>
