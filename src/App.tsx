9efef17f170390463a89bcf51c78dc469a363248
fekoneko <55813967+fekoneko@users.noreply.github.com>
2024-08-05 13:48:26 +0300

    window fullscreen mode

--- src/App.tsx
+++ src/App.tsx
@@ -8,20 +8,29 @@ import { SearchQueryProvider } from '@/providers/SearchQueryProvider';
 import { Header } from '@/components/header';
 import { CollectionExplorer } from '@/components/collection-explorer';
 import { FavoriteButton, ThemeButton } from '@/components/hover-buttons';
+import { useKeyboardEvent } from '@/hooks';
+import { appWindow } from '@tauri-apps/api/window';
 
-export const App: FC = () => (
-  <ThemeProvider>
-    <OutputProvider>
-      <CollectionProvider>
-        <SearchQueryProvider>
-          <Header />
+export const App: FC = () => {
+  useKeyboardEvent('keydown', 'F11', async () => {
+    const isFullscreen = await appWindow.isFullscreen();
+    appWindow.setFullscreen(!isFullscreen);
+  });
 
-          <CollectionExplorer />
+  return (
+    <ThemeProvider>
+      <OutputProvider>
+        <CollectionProvider>
+          <SearchQueryProvider>
+            <Header />
 
-          <FavoriteButton />
-          <ThemeButton />
-        </SearchQueryProvider>
-      </CollectionProvider>
-    </OutputProvider>
-  </ThemeProvider>
-);
+            <CollectionExplorer />
+
+            <FavoriteButton />
+            <ThemeButton />
+          </SearchQueryProvider>
+        </CollectionProvider>
+      </OutputProvider>
+    </ThemeProvider>
+  );
+};
