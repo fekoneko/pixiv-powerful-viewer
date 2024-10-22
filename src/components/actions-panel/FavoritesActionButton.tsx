ade54cd8254ab6e11286fa1fe08202c6792e5941
fekoneko <55813967+fekoneko@users.noreply.github.com>
2024-10-23 00:56:57 +0300

    tab indexes for action buttons

--- src/components/actions-panel/FavoritesActionButton.tsx
+++ src/components/actions-panel/FavoritesActionButton.tsx
@@ -32,6 +32,7 @@ export const FavoritesActionButton: FC = () => {
     <button
       role="button"
       onClick={toggleFavorites}
+      tabIndex={3}
       className="order-3 flex size-10 items-center justify-center rounded-md border border-border bg-paper px-[0.58rem] py-2 text-lg shadow-md hover:bg-paper-hover focus:bg-paper-hover focus:outline-none"
     >
       {searchQuery === '#favorites' ? '❌' : '⭐'}
--- src/components/actions-panel/ThemeActionButton.tsx
+++ src/components/actions-panel/ThemeActionButton.tsx
@@ -7,6 +7,7 @@ export const ThemeActionButton: FC = () => {
   return (
     <button
       onClick={toggleTheme}
+      tabIndex={2}
       className="order-2 flex size-10 items-center justify-center rounded-md border border-border bg-paper px-[0.58rem] py-2 text-lg shadow-md hover:bg-paper-hover focus:bg-paper-hover focus:outline-none"
     >
       {theme === 'dark' ? '🌙' : '💡'}
